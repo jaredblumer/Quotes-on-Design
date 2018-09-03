ac049de7d6ac1965d076a6a13fd8151f5619c1e2
Jared Blumer <jaredgblumer@gmail.com>
2018-09-03 17:20:45 -0400

    Add on-ready quote request

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
-$('#new-quote').on('click', function(e) {
-  e.preventDefault();
+
+// Retrieve Quote When Document is Ready
+$( document ).ready(function() {
   $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
 
   function(response) {
-    console.log(response);
+    var result = response.shift();
+    console.log(result);
+    $('#quote-text').html(result.content);
+    $('#author-text').html(result.title);
+  });
+});
+
+// Retrieve New Quote
+$('#new-quote').on('click', function(event) {
+  event.preventDefault();
+  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+
+  function(response) {
+    var result = response.shift();
+    console.log(result);
+    $('#quote-text').html(result.content);
+    $('#author-text').html(result.title);
   });
 });
