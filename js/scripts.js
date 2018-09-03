84028529d5d4a244d06bef03400824569aa1b6fd
Jared Blumer <jaredgblumer@gmail.com>
2018-09-03 18:29:52 -0400

    Add dynamic twitter web intent link and disabled cache

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,42 @@
 
-// Retrieve Quote When Document is Ready
-$( document ).ready(function() {
-  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+// Request Quote, Populate Page, & Update Twitter URL
+function getQuote() {
+  $.ajax({
+    cache: false,
+    url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+    dataType: "json",
+    success:
+    function(response) {
+      var result = response.shift();
+      var content = result.content;
+      var quote = $(content).text().trim();
+      var author = result.title.trim();
+      console.log(result);
+      console.log(quote);
 
-  function(response) {
-    var result = response.shift();
-    console.log(result);
-    $('#quote-text').html(result.content);
-    $('#author-text').html(result.title);
+      // Format Twitter Web Intent URL
+      var twitterQuote = '"' + quote + '"' + " —" + author;
+      var twitterURL = "https://twitter.com/intent/tweet?text=" + encodeURI(twitterQuote);
+      $("#twitter-link").attr("href", twitterURL);
+      console.log(twitterURL);
+
+      // Display Quote and Author
+      $("#quote-text").text(quote);
+      $("#author-text").text(author);
+    },
+    error:
+    function() {
+      $("#quote-text").text("Whoops. 404 Error. Please try again later.");
+      $("#author-text").text("The Internet");
+    }
   });
-});
+}
+
+// Retrieve Quote When Document is Ready
+$( document ).ready(getQuote());
 
 // Retrieve New Quote
-$('#new-quote').on('click', function(event) {
+$("#new-quote").on("click", function(event) {
   event.preventDefault();
-  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
-
-  function(response) {
-    var result = response.shift();
-    console.log(result);
-    $('#quote-text').html(result.content);
-    $('#author-text').html(result.title);
-  });
+  getQuote();
 });
